f7784d837b8caf4025d8c9edff8d86888d8af2c5
FutureWiz2002 <jabirm2002@gmail.com>
2022-08-09 16:02:05 +0600

    Update Linear Regression Basics.py

--- Machine Learning/Linear Regression Basics.py
+++ Machine Learning/Linear Regression Basics.py
@@ -20,7 +20,7 @@ def get_gradient_at_m(x, y, b, m):
   m_gradient = -(2/N) * diff  
   return m_gradient
 
-#Your step_gradient function here
+#step_gradient function here
 def step_gradient(b_current, m_current, x, y, learning_rate):
     b_gradient = get_gradient_at_b(x, y, b_current, m_current)
     m_gradient = get_gradient_at_m(x, y, b_current, m_current)
@@ -28,7 +28,7 @@ def step_gradient(b_current, m_current, x, y, learning_rate):
     m = m_current - (learning_rate * m_gradient)
     return [b, m]
   
-#Your gradient_descent function here:  
+#gradient_descent function here:  
 def gradient_descent(x, y, learning_rate, num_iteration):
   b , m = 0, 0
   for i in range(num_iteration):
@@ -38,10 +38,9 @@ def gradient_descent(x, y, learning_rate, num_iteration):
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
 
-#Uncomment the line below to run your gradient_descent function
+
 b, m = gradient_descent(months, revenue, 0.01, 1000)
 
-#Uncomment the lines below to see the line you've settled upon!
 y = [m*x + b for x in months]
 
 plt.plot(months, revenue, "o")
